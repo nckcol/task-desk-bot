596367f27824a30b29d017c59ece2f46cb3eb616
Nikita Popov <nckcol@gmail.com>
2018-01-19 21:33:50 +0200

    feat: add env file and test code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+const dotenv = require('dotenv')
+const Telegraf = require('telegraf')
+
+dotenv.config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((ctx) => {
+  console.log('started:', ctx.from.id)
+  return ctx.reply('Welcome!')
+})
+bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.startPolling()
